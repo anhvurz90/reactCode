6508ad956395adf16b4413b6bc6711d141d9384c
anhvurz90 <anhvurz90@gmail.com>
2017-01-20 16:16:51 +0700

    Add #03.React Components

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
-alert("It wooooorks!");
+var React = require("react");
+var ReactDom = require("react-dom");
+
+//Create component
+var TodoComponent = React.createClass({
+  render: function() {
+      return (
+        <h1>Hellooooooooooooooooo</h1>
+      );
+  }
+});
+//Put component into html page
+ReactDom.render(<TodoComponent/>, document.getElementById("todo-wrapper"));
